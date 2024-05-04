1d0f751ec97fddb18a723239965ddc2732be441d
Ubuntu <ubuntu@ip-172-31-16-232.ap-southeast-1.compute.internal>
2024-05-04 09:15:26 +0000

    update main.tf

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+resource "aws_instance" "example_server" {
+  ami           = "ami-04e914639d0cca79a"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "JacksBlogExample"
+  }
+}
